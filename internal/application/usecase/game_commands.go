b724943d2398601754a042c98c9a44e3241bc28c
Fellipe Leonardo <112525075+neoRandom@users.noreply.github.com>
2026-08-15 22:24:57 -0300

    chore: enhance game name and ID validation

--- internal/application/usecase/game_commands.go
+++ internal/application/usecase/game_commands.go
@@ -17,18 +17,33 @@ func NewGameCommands() *GameCommands {
 
 func (uc *GameCommands) AddGame(ctx context.Context, name string) (database.Game, error) {
 	if len(name) < 3 {
-		return database.Game{}, fmt.Errorf("game name shorter than 3 characters: '%s'", name)
+		return database.Game{}, fmt.Errorf("Game name cannot have less than 3 characters: '%s'", name)
 	}
 
 	if !util.IsAlphanumeric(name) {
-		return database.Game{}, fmt.Errorf("game name isn't pure alphanumeric: '%s'", name)
+		return database.Game{}, fmt.Errorf("Game name isn't purely alphanumeric: '%s'", name)
 	}
 
 	return uc.q.CreateGame(ctx, name)
 }
 
 func (uc *GameCommands) UpdateGame(ctx context.Context, id int64, name string) error {
-	return nil
+	if id < 1 {
+		return fmt.Errorf("Invalid game ID: %d", id)
+	}
+	
+	if len(name) < 3 {
+		return fmt.Errorf("New game name cannot have less than 3 characters: '%s'", name)
+	}
+
+	if !util.IsAlphanumeric(name) {
+		return fmt.Errorf("New game name isn't purely alphanumeric: '%s'", name)
+	}
+
+	return uc.q.UpdateGame(ctx, database.UpdateGameParams{
+		ID: id,
+		Name: name,
+	})
 }
 
 func (uc *GameCommands) DeleteGame(ctx context.Context, id int64) error {
