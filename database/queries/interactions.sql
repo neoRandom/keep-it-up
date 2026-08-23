14139121256392302f43f6d8e1202f891b558088
Fellipe Leonardo <112525075+neoRandom@users.noreply.github.com>
2026-08-23 12:46:18 -0300

    chore: remove unused AddInteraction query

--- database/queries/interactions.sql
+++
@@ -1,9 +0,0 @@
--- name: AddInteraction :one
-INSERT INTO interactions (
-    game_id,
-    player_id,
-    action,
-    occurred_at
-)
-VALUES (?, ?, ?, ?)
-RETURNING id, game_id, player_id, action, occurred_at;
